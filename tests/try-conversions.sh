6edbea36dd0e96442f94b42bbc280af9460b3831
Pieter Edelman <edelman@nictiz.nl>
2019-11-01 14:58:58 +0100

    Make the search for ADA instances relative to the test folder

--- tests/try-conversions.sh
+++ tests/try-conversions.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
-ada_instances=$(find .. -path '../ada_2_fhir/*/ada_instance/*.xml') # Limit to ada_2_fhir
+scriptdir=$(realpath --relative-to $(pwd) $(dirname $0))
+output_dir=$scriptdir/output
+mkdir -p $output_dir
+ada_instances=$(find $scriptdir/.. -path '*ada_2_fhir/*/ada_instance/*.xml') # Limit to ada_2_fhir
 for instance_path in $ada_instances; do
   instance_file=$(basename $instance_path)
-  project_folder=$(dirname $instance_path)/..
-  transformation_paths=
+  project_folder=$(realpath --relative-to $(pwd) $(dirname $instance_path)/..)
   for transformation_path in $(find $project_folder/payload/ -name *.xsl); do
-    echo "Transforming $instance_file with $transformation_path"
-    java -jar /usr/share/java/Saxon-HE.jar -quit:on -warnings:recover -s:$instance_path -xsl:$transformation_path > /dev/null
+    echo "Transforming $instance_file with $(basename $transformation_path) in $project_folder"
+    java -jar /usr/share/java/Saxon-HE.jar -quit:on -warnings:recover -s:$instance_path -xsl:$transformation_path -o $output_dir > /dev/null
   done
 done
+rm -r $output_dir
