9b4b13629e257f231621a59087a662b945e48e77
Arianne van de Wetering <wetering@nictiz.nl>
2020-06-16 13:59:25 +0200

    ada_2_fhir cio, regenerated after update ada instance for MM-717 and MM-1194

---
+++ ada_2_fhir/cio/1.0.0/ant-setup-and-build-cio-100-ada2fhir.bat
@@ -0,0 +1,9 @@
+@setlocal enableextensions
+
+@echo off
+
+echo.ant cio 100 ada2fhir setup...
+call ant -f _ant-buildfiles\ant-publish\build-ada2fhir-cio-100.xml setup >ant-setup.log
+
+echo.Done
+pause
