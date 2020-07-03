20894e78c28381761690a5eccc23dd47e36561e8
Arianne van de Wetering <wetering@nictiz.nl>
2020-07-03 16:41:50 +0200

    ada 2 fhir. MP 907 and 910. Added ant scripts for MP 910. Regenerated both MP 907 and MP 910.

--- ada_2_fhir/mp/9.0.7/ant-setup-and-build-mp-907-ada2fhir.bat
+++ ada_2_fhir/mp/9.0.7/ant-setup-and-build-mp-907-ada2fhir.bat
@@ -3,7 +3,7 @@
 @echo off
 
 echo.ant mp 907 ada2fhir setup...
-call ant -f _ant-buildfiles\ant-publish\build-ada2fhir-mp-907.xml setup >ant-setup.log
+call ant -f _ant-buildfiles\ant-publish\build-ada2fhir-mp-907.xml setup_907 >ant-setup.log
 
 echo.Done
 pause
