3cd5ce331e7d81c002481f54f8fc279f77735454
Arianne van de Wetering <wetering@nictiz.nl>
2021-10-06 13:50:33 +0200

    ada_2_hl7. MP9 2.0. schema and schematron validation automation using ant.

--- ada_2_hl7/mp/9.2.0/ant-validate-mp-920-ada2hl7.bat
+++
@@ -1,9 +0,0 @@
-@setlocal enableextensions
-
-@echo off
-
-echo.ant ada2hl7 MP 9 2.0 validate ...
-call ant -f _ant-buildfiles\ant-publish\build-ada2hl7-mp-920.xml validate_hl7_920>ant-validate.log
-
-echo.Done
-pause
