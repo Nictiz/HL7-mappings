7efeebce8a1864960b07cf1aaeafa9f4fd460f09
wim <info@wimdevriesitservices.nl>
2022-01-14 18:39:48 +0100

    some path changes and ant script

---
+++ ada_2_ada/mp/9.2.0/ant-build-ada2ada.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo ant mp 920 fhir2ada-r4 build...
+ant -f ./_ant-buildfiles/ant-publish/build-ada2ada-mp-include.xml > ant-build.log
+echo Done
+
+read -p "Press any key to resume ..."
