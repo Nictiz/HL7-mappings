aefdb190db506abc0a1101fa0b74b910319f52b2
Arianne van de Wetering <wetering@nictiz.nl>
2022-04-29 18:46:54 +0200

    ada_2_hl7. Regenerated MP907 and MP9 2.0 for regression testing after merge with master.

--- ada_2_hl7/mp/9.2.0/ant-schematronvalidate-results-mp-920-ada2hl7.bat
+++ ada_2_hl7/mp/9.2.0/ant-schematronvalidate-results-mp-920-ada2hl7.bat
@@ -2,8 +2,8 @@
 
 @echo off
 
-echo.ant ada2hl7 MP 9 2.0 schematronvalidate ...
-call ant -f _ant-buildfiles\ant-publish\build-ada2hl7-mp-920.xml schematronvalidate_hl7_920>ant-schematronvalidate.log
+echo.ant ada2hl7 MP 9 2.0 schematronvalidate results...
+call ant -f _ant-buildfiles\ant-publish\build-ada2hl7-mp-920.xml schematronresults_hl7_920 > ant-schematronvalidateresults.log
 
 echo.Done
 pause
