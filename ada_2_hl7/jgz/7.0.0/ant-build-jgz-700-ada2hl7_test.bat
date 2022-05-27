1ba2e937fcb74449f50ca7686aebc2e649a86247
Onno <onno.gieling@gmail.com>
2022-05-27 12:35:05 +0200

    Add script for running testfiles to exclude output from tracking

---
+++ ada_2_hl7/jgz/7.0.0/ant-build-jgz-700-ada2hl7_test.bat
@@ -0,0 +1,9 @@
+@setlocal enableextensions
+
+@echo off
+
+echo.ant ada2hl7 JGZ 7.0.0 build...
+call ant -f _ant-buildfiles\ant-publish\build-ada2hl7-jgz-700.xml convert_ada_2_hl7_700_test >ant-build.log
+
+echo.Done
+pause
