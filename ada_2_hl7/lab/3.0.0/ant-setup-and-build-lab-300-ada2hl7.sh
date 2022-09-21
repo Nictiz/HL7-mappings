46a62b03b9fc5cf750ca17081797b6dbffe598a4
Alexander Henket <henket@nictiz.nl>
2022-09-21 08:33:42 -0400

    Build improvements

--- ada_2_hl7/lab/3.0.0/ant-setup-and-build-lab-300-ada2hl7.sh
+++ ada_2_hl7/lab/3.0.0/ant-setup-and-build-lab-300-ada2hl7.sh
@@ -1,6 +1,6 @@
 echo "ant lab 3.0.0 ada2hl7 mapping" 
 echo "setup ..."
-ant -Drun-ivy=false -f `dirname $0`/_ant-buildfiles/ant-publish/build-ada2hl7-lab-300.xml setup_lab300 setup convert_ada_2_hl7_300 schemavalidate_hl7_300 schematronvalidate_hl7_300 convert_ada_2_hl7_examples_300 2>&1 | tee `dirname $0`/ant-setup.log
+ant -Drun-ivy=false -f `dirname $0`/_ant-buildfiles/ant-publish/build-ada2hl7-lab-300.xml setup_lab300 convert_ada_2_hl7_300 schemavalidate_hl7_300 schematronvalidate_hl7_300 convert_ada_2_hl7_examples_300 2>&1 | tee `dirname $0`/ant-setup.log
 
 #echo "build ada2hl7..."
 #ant -f `dirname $0`/_ant-buildfiles/ant-publish/build-ada2hl7-lab-300.xml convert_ada_2_hl7_300 2>&1 | tee `dirname $0`/ant-setup.log
