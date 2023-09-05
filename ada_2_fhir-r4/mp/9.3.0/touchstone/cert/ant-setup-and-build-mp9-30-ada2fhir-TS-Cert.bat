7f0cfce9666943162a5cb2af6c1494e2ab835b3c
Arianne van de Wetering <wetering@nictiz.nl>
2023-09-05 16:15:50 +0200

    ada_2_fhir-r4. MP9 3.0. Regenerated instances.

---
+++ ada_2_fhir-r4/mp/9.3.0/touchstone/cert/ant-setup-and-build-mp9-30-ada2fhir-TS-Cert.bat
@@ -0,0 +1,11 @@
+@setlocal enableextensions
+
+@echo off
+
+echo.ant mp9 30 ada2fhir-r4 Touchstone Test setup ...
+call ant -f _ant-buildfiles\ant-publish\build-ada2fhir-mp9-30-TS-Cert.xml setup_30 >ant-setup.log
+echo.Done
+echo.ant mp9 30 ada2fhir-r4 Touchstone Test build ...
+call ant -f _ant-buildfiles\ant-publish\build-ada2fhir-mp9-30-TS-Cert.xml convert_ada_2_fhir_30 >ant-build.log
+echo.Done
+pause
