61e85a3f67f67e1132a9f0f1ef35ec3d96fbeacb
Onno Gieling <onno.gieling@gmail.com>
2024-05-08 09:26:00 +0200

    Update wip validate jgz script

--- ada_2_hl7/jgz/util/validate_xml.sh
+++ ada_2_hl7/jgz/util/validate_xml.sh
@@ -10,18 +10,29 @@ export dirSaxon6=${wd}/lib/saxon-6
 
 export xmlFiles=${wdxml}/*
 
-echo " `date`"
-
 # Get the folders of the JGZ versions available
 folders_version=()
-for input_version in "$@"; do
+arguments=()
+if [[ ${#@} -gt "0" ]]; then
+    arguments=$@
+else
+    arguments=('all')
+fi
+
+if [[  $1 == "help" || $1 == "man" ]]
+then
+    echo "Pass the versions for validation as arguments. No argument will validate all versions"
+    exit 1 
+fi
+
+for input_version in ${arguments[@]}; do
     for f in $wd/../*
     do
         export folder=$f
         export f_folder=$(basename -- "$folder")
         # Get the folders of the JGZ versions available
         if [[ "$f_folder" != "util" ]]; then
-            if [[ $input_version == "" ]]
+            if [[ $input_version == "all" ]]
             then
                 folders_version+=($folder)
             fi
@@ -33,19 +44,42 @@ for input_version in "$@"; do
     done
 done
 
-if [[ ${#folders_version[@]} != ${#@} ]]
+# Exit if there is no folder found for the versions in the arguments 
+if [[  ${#@} -gt 0 && ${#folders_version[@]} != ${#@} ]]
 then
     folders_version+=($folder)
     echo "Version not found"
     exit 1 
 fi
 
-echo "Validate version(s) ${folders_version[@]}"
+#echo "Validate version(s) ${folders_version[@]}"
 
-for f in "${folders_version[@]}"
+# Get all the SVRL and use case directories
+for f_version in "${folders_version[@]}"
 do
-    echo get files in $f
+    #echo get files in $f_version
+    # check if the SVRLs are present
+    d_svrl=()
+    for version_content in $wd/$f_version/*
+    do
+        DIRECTORY=$version_content
+        d_content=$(basename -- "$DIRECTORY")
+        if [[ $d_content == hl7*_svrl ]] ;then
+            d_svrl+=($d_content)
+        fi
+        if [[ -d $version_content/hl7_instance ]]; then
+            echo usecase: $d_content 
+            d_usecase=$version_content
+        fi
+    done
+    echo SVRLs: ${d_svrl[@]}
 done
+
+# Get the svrl files
+
+# Get the HL7 instances
+
+# Validate the HL7 instances with the corresponding svrl and convert to HTML
 
 
 
