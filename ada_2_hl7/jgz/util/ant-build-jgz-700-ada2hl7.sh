40103d924691a9b8a4d74b013ad404c5dfbaa271
Onno Gieling <onno.gieling@gmail.com>
2024-10-17 18:32:11 +0200

    Remove JGZ util from HL7 mappings

--- ada_2_hl7/jgz/util/ant-build-jgz-700-ada2hl7.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/zsh
-#
-echo ant ada2hl7 JGZ 7.0.0 build...
-ant -f '../dossieroverdracht/_ant-buildfiles/ant-publish/build-ada2hl7-jgz-700.xml'
-# >`dirname $0`/ant-build.log
-#
-echo Done
--- ada_2_hl7/jgz/util/ant-setup-jgz-700-ada2hl7.bat
+++
@@ -1,9 +0,0 @@
-@setlocal enableextensions
-
-@echo off
-
-echo.ant ada2hl7 jgz 7.0.0 setup...
-call ant -f _ant-buildfiles\ant-publish\build-ada2hl7-jgz-700.xml setup_jgz700 >ant-setup.log
-
-echo.Done
-pause
