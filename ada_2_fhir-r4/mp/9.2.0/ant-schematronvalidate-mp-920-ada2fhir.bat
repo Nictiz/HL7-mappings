4d29c80f7ad0c64cb877454f29f1005534a9fb14
Erik Siegel <erik@xatapult.nl>
2024-11-19 10:04:34 +0100

    Removed all scripting from ada-2-fhir-r4.

--- ada_2_fhir-r4/mp/9.2.0/ant-schematronvalidate-mp-920-ada2fhir.bat
+++
@@ -1,9 +0,0 @@
-@setlocal enableextensions
-
-@echo off
-
-echo.ant mp 920 ada2fhir-r4 schematronvalidate...
-call ant -f _ant-buildfiles\ant-publish\build-ada2fhir-mp-920.xml schematronvalidate_fhir_920>ant-schematronvalidate.log
-
-echo.Done
-pause
--- ada_2_fhir-r4/mp/9.3.0/touchstone/test/ant-setup-mp9-30-ada2fhir-TS-Test.bat
+++
@@ -1,8 +0,0 @@
-@setlocal enableextensions
-
-@echo off
-
-echo.ant mp9 30 ada2fhir-r4 Touchstone Test setup ...
-call ant -f _ant-buildfiles\ant-publish\build-ada2fhir-mp9-30-TS-Test.xml setup_30 >ant-setup.log
-echo.Done
-pause
