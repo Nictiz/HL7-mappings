dd2fd2b7ad04012c9ab0b2a371bcc5ea374ce2fe
Arianne van de Wetering <wetering@nictiz.nl>
2024-12-29 16:46:33 +0100

    Mp 1817 (#335)
    MP-1817. 
    * ada-2-hl7. MP9 3.0. Improve extension url for herhaalperiode cyclisch schema.Validated (success)
    * ada-2-hl7. MP 9.2.0. Just a temp fix to make sure the XSL compiles. However MP 9.1 and 9.2 are on the nomination for deletion.
    * ada-2-hl7. MP 907. Regenerated with new ada instances: TDate and instructiontext now handled in adarefs2ada. Tdate set to 1/1/2024
    * hl7-2-ada. MP 907 regression fixes. Refreshed and regenerated.
    * ada-2-fhir. MP907. Refreshed and regenerated.
    * hl7-2-ada. MP9 3.0. Regression: gebruiksinstructie specials. Also added some batch files for efficiency purposes. Regenerated.
    * ada-2-hl7 MP9 3.0. Revalidated, created NICTIZ issue for oversized id/@extensions in HYB
    * ada-2-fhir-r4. MP9 3.0. Refreshed and regenerated.

--- hl7_2_ada/mp/9.3.0/ant-roundtrip-mp-930-hl72ada2hl7.bat
+++ hl7_2_ada/mp/9.3.0/ant-roundtrip-mp-930-hl72ada2hl7.bat
@@ -3,6 +3,9 @@
 @echo off
 
 echo.ant MP9 3.0 hl72ada2hl7 roundtrip...
+echo.ant MP9 3.0 hl72ada build...
+call ant -f _ant-buildfiles\ant-publish\build-hl72ada-mp-930.xml convert_hl7_2_ada_930 >ant-build.log
+echo.ant MP9 3.0 ada2hl7 roundtrip...
 call ant -f _ant-buildfiles\ant-publish\build-hl72ada-mp-930.xml roundtrip_hl7_2_ada_930 >ant-roundtrip.log
 
 echo.Done
