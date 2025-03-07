66f6b11ac4bd0fac88244b5b86f7fc40eed84ce8
aboogaarnictiz <annemarie.boogaard@nictiz.nl>
2025-03-07 09:36:04 +0100

    clean up hl7-2-ada

--- hl7_2_ada/cio/1.0.0/ant-build-cio-100-hl72ada.bat
+++
@@ -1,9 +0,0 @@
-@setlocal enableextensions
-
-@echo off
-
-echo.ant cio 100 hl72ada build...
-call ant -f _ant-buildfiles\ant-publish\build-hl72ada-cio-100.xml convert_hl7_2_ada >ant-build.log
-
-echo.Done
-pause
