e367a04028a12b95dc04f4e479cded4968224fa5
Alexander Henket <henket@nictiz.nl>
2025-08-25 16:45:41 +0200

    Latest run with latest GZ and zib2017 base profile updates applied

--- ada_2_fhir/peri/3.2.6/validate/_validate.sh
+++ ada_2_fhir/peri/3.2.6/validate/_validate.sh
@@ -19,7 +19,9 @@ export JAVA_TOOL_OPTIONS="$JAVA_TOOL_OPTIONS -Dfile.encoding=UTF-8"
 publisher=$input_cache_path/$publisher_jar
 if test -f "$publisher"; then
 	#java -jar $publisher -ig . $txoption $*
-	java -jar $publisher -ig `dirname $0`/../../../../../Geboortezorg-STU3/profiles -ig nictiz.fhir.nl.stu3.zib2017#2.2.10 -version 3.0 -recurse -debug -html-output validation.html -txLog txlog.txt -locale nl -language nl -sct 11000146104 -display-issues-are-warnings -level error `dirname $0`/../med_mij_uitbreiding_verloskunde_beschikbaarstellen/fhir_instance
+      #java -jar $publisher -ig nictiz.fhir.nl.stu3.geboortezorg#1.3.3 -ig nictiz.fhir.nl.stu3.zib2017#2.2.10 -version 3.0 -recurse -debug -html-output validation.html -txLog txlog.txt -locale nl -language nl -sct 11000146104 -display-issues-are-warnings -level error `dirname $0`/../med_mij_uitbreiding_verloskunde_beschikbaarstellen/fhir_instance
+      #java -jar $publisher -ig `dirname $0`/../../../../../Geboortezorg-STU3/profiles -ig nictiz.fhir.nl.stu3.zib2017#2.2.10 -version 3.0 -recurse -debug -html-output validation.html -txLog txlog.txt -locale nl -language nl -sct 11000146104 -display-issues-are-warnings -level error `dirname $0`/../med_mij_uitbreiding_verloskunde_beschikbaarstellen/fhir_instance
+	java -jar $publisher -ig `dirname $0`/../../../../../Geboortezorg-STU3/profiles -ig `dirname $0`/../../../../../Nictiz-STU3-Zib2017/Profiles\ -\ ZIB\ 2017/ -version 3.0 -recurse -debug -html-output validation.html -txLog txlog.txt -locale nl -language nl -sct 11000146104 -display-issues-are-warnings -level error `dirname $0`/../med_mij_uitbreiding_verloskunde_beschikbaarstellen/fhir_instance
 
 else
 	publisher=../$publisher_jar
