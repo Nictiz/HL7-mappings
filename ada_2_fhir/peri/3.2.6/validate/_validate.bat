5ae99304fdc7259971fd7b3a6c9b4f0002a31ec8
TessaVanAlphen <tessa.vanalphen@nictiz.nl>
2026-07-31 11:34:16 +0200

    fixtures validaton results

--- ada_2_fhir/peri/3.2.6/validate/_validate.bat
+++ ada_2_fhir/peri/3.2.6/validate/_validate.bat
@@ -16,12 +16,48 @@ SET txoption=
 
 SET JAVA_TOOL_OPTIONS=-Dfile.encoding=UTF-8
 
+SET "instance_root=%~dp0..\med_mij_uitbreiding_verloskunde_beschikbaarstellen\fhir_instance"
+SET "echo_root=%~dp0..\med_mij_echo_beschikbaarstellen\fhir_instance"
+SET "kraam_root=%~dp0..\med_mij_kraam_beschikbaarstellen\fhir_instance"
+
 IF EXIST "%input_cache_path%\%publisher_jar%" (
 	REM JAVA -jar "%input_cache_path%\%publisher_jar%" -ig . %txoption% %*
-  java -jar "%input_cache_path%\%publisher_jar%" -ig nictiz.fhir.nl.stu3.geboortezorg#1.3.3 -ig nictiz.fhir.nl.stu3.zib2017#2.2.10 -version 3.0 -recurse -debug -html-output validation.html -txLog txlog.txt -locale nl -language nl -sct 11000146104 -display-issues-are-warnings %~dp0/../med_mij_uitbreiding_verloskunde_beschikbaarstellen/fhir_instance
+  	java -jar "%input_cache_path%\%publisher_jar%" ^
+   		-ig nictiz.fhir.nl.stu3.geboortezorg#3.0.0-beta1-rc.2 ^
+   		-ig nictiz.fhir.nl.stu3.zib2017#2.2.10 ^
+   		-version 3.0 ^
+   		-recurse -html-output validation.html ^
+   		-txLog txlog.txt ^
+   		-locale nl -language nl ^
+   		-sct 11000146104 ^
+  		%txoption% ^
+   		-display-issues-are-warnings ^
+    	"%instance_root%\gz-izv-kwa-casus1" ^
+   		"%instance_root%\gz-izv-kwa-casus2" ^
+		"%instance_root%\gz-izv-kwa-casus4" ^
+		"%echo_root%\gz-ize-kwa-casus1" ^
+		"%kraam_root%\gz-izk-kwa-casus1" ^
+		 > validator-console.log 2>&1
 ) ELSE If exist "..\%publisher_jar%" (
 	REM JAVA -jar "..\%publisher_jar%" -ig . %txoption% %*
-  java -jar "..\%publisher_jar%" -ig nictiz.fhir.nl.stu3.geboortezorg#1.3.3 -ig nictiz.fhir.nl.stu3.zib2017#2.2.10 -version 3.0 -recurse -debug -html-output validation.html -txLog txlog.txt -locale nl -language nl -sct 11000146104 -display-issues-are-warnings %~dp0/../med_mij_uitbreiding_verloskunde_beschikbaarstellen/fhir_instance
+	java -jar "..\%publisher_jar%" ^
+        -ig nictiz.fhir.nl.stu3.geboortezorg#3.0.0-beta1-rc.2 ^
+        -ig nictiz.fhir.nl.stu3.zib2017#2.2.10 ^
+        -version 3.0 ^
+        -recurse ^
+        -html-output validation.html ^
+        -txLog txlog.txt ^
+        -locale nl ^
+        -language nl ^
+        -sct 11000146104 ^
+        -display-issues-are-warnings ^
+        %txoption% ^
+    	"%instance_root%\gz-izv-kwa-casus1" ^
+   		"%instance_root%\gz-izv-kwa-casus2" ^
+		"%instance_root%\gz-izv-kwa-casus4" ^
+		"%echo_root%\gz-ize-kwa-casus1" ^
+		"%kraam_root%\gz-izk-kwa-casus1" ^
+		 > validator-console.log 2>&1
 ) ELSE (
 	ECHO IG Validator NOT FOUND in input-cache or parent folder.  Please run _updateValidator.  Aborting...
 )
